dc7c0e4b76c130a9404bea8b6c12adef6d560d13
Sebastian Castro Pavez <castropavezs@gmail.com>
2024-04-17 14:12:49 -0400

    cv

--- src/components/Downland/Downland.jsx
+++ src/components/Downland/Downland.jsx
@@ -5,7 +5,7 @@ const Downland = () => {
     <div className="downland-container">
         <span className="text-downland">Puedes Descargar mi CV</span>
         <a
-          href="src\assets\cv\Sebastián_Castro_Pavez.pdf"
+          href="https://res.cloudinary.com/dp1c36xd0/image/upload/v1713377508/Portfolio%20IMG/cv/zmlyrtbivcecvvuc5kqu.pdf"
           download="Sebastián_Castro_Pavez_(Full-Stack-Web-Developer).pdf"
           className="downland-CV"
         >
